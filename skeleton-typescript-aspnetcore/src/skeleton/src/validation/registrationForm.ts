7fa3b4a5802c447c0a7c0517b014b4cf21e7f368
Martin Mason <Martin@softwaremasons.com>
2016-12-19 20:51:30 -0600

    integrate validation
    created a sample Registration route that demonstrates how aurelia-integration can be integrated into the skeleton-typescript-aspnetcore project with covering unit tests

---
+++ skeleton-typescript-aspnetcore/src/skeleton/src/validation/registrationForm.ts
@@ -0,0 +1,39 @@
+﻿import { inject } from 'aurelia-dependency-injection';
+import { ValidationControllerFactory, ValidationController, ValidationRules, ControllerValidateResult, ValidateResult, Validator } from 'aurelia-validation';
+
+@inject(ValidationControllerFactory)
+export class RegistrationForm {
+  heading: string = 'Registration';
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  controller: ValidationController = null;
+  errorMessage: string = '';
+  validator: Validator = null;
+
+  constructor(controllerFactory: ValidationControllerFactory, validator: Validator) {
+    this.controller = controllerFactory.createForCurrentScope();
+    this.validator = validator;
+
+    ValidationRules
+      .ensure('firstName').required().withMessage('First Name property is required.')
+      .ensure('lastName').required().withMessage('Last Name property is required.')
+      .ensure('email').required().email()
+      .on(this);
+ }
+
+  submit() {
+    this.controller.validate().then((result: ControllerValidateResult) => {
+      if (result.valid) {
+        this.errorMessage = '';
+      } else {
+        for (let item: number = 0; item < result.results.length; item++) {
+          let validateResult: ValidateResult = result.results[item];
+          if (!validateResult.valid) {
+            this.errorMessage = validateResult.message;
+          }
+        }
+      }
+    });
+  }
+}
---
+++ skeleton-typescript-aspnetcore/src/skeleton/test/unit/validation/registrationForm.spec.ts
@@ -0,0 +1,64 @@
+﻿import { RegistrationForm } from '../../../src/validation/registrationForm';
+import { Container } from 'aurelia-dependency-injection';
+import { ValidationControllerFactory, Validator, ValidateResult } from 'aurelia-validation';
+import { Aurelia } from 'aurelia-framework';
+import { bootstrap } from 'aurelia-bootstrapper';
+
+describe('RegistrationForm', () => {
+  let container: Container;
+  let sut: RegistrationForm;
+
+  beforeEach((done: () => void) => {
+    bootstrap((aurelia: Aurelia) => {
+      container = aurelia.container;
+
+      aurelia.use
+        .standardConfiguration()
+        .plugin('aurelia-validation');
+
+      return aurelia.start();
+    }).then(() => {
+      let validationControllerFactory = container.get(ValidationControllerFactory);
+      let validator = container.get(Validator);
+      sut = new RegistrationForm(validationControllerFactory, validator);
+      done();
+    });
+  });
+
+  describe('firstName', () => {
+    it('validation fails when blank', (done: () => void) => {
+      sut.firstName = '';
+      sut.validator.validateProperty(sut, 'firstName').then((results: ValidateResult[]) => {
+        expect(results[0].message).toBe('First Name property is required.');
+        done();
+      });
+    });
+
+    it('validation passes when provided', ((done: () => void) => {
+      sut.firstName = 'Valid first name';
+      sut.validator.validateProperty(sut, 'firstName').then((results: ValidateResult[]) => {
+        expect(results[0].valid).toBe(true);
+        done();
+      });
+    }));
+  });
+
+  describe('lastName', () => {
+    it('validation fails when blank', (done: () => void) => {
+      sut.lastName = '';
+      sut.validator.validateProperty(sut, 'lastName').then((results: ValidateResult[]) => {
+        expect(results[0].message).toBe('Last Name property is required.');
+        done();
+      });
+    });
+
+    it('validation passes when provided', ((done: () => void) => {
+      sut.lastName = 'Valid last name';
+      sut.validator.validateProperty(sut, 'lastName').then((results: ValidateResult[]) => {
+        expect(results[0].valid).toBe(true);
+        done();
+      });
+    }));
+  });
+});
+
